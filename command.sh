95695929c99ae5dac26582528aaca56f850d4fdb
Johan Lindfors <johan.lindfors@coderox.se>
2024-09-12 15:02:02 +0200

    Now using cool script

---
+++ command.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+   fi
+
+  shift
+done
+
+echo Now doing work with secret token passed in arguments
